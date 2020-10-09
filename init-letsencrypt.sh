40214cdb7712524d6a91af43a3b6dbd9220b6821
Nathan Beddoe <nbedd2@protonmail.com>
2020-10-10 09:29:21 +1000

    stop docker-composing

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -33,7 +33,7 @@ mkdir -p "$data_path/conf/live/$domains"
 docker-compose run --rm --entrypoint "\
   openssl req -x509 -nodes -newkey rsa:1024 -days 1\
     -keyout '$path/privkey.pem' \
-    -out '$path/fullchain.pem' \
+    -out '$path/fullchain.pem' \ubdmo
     -subj '/CN=localhost'" certbot
 echo
 
